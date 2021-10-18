1a6d3d44472fb24b723f15abcdab99877504bc38
zbwxp <zbwxpau@gmail.com>
2021-10-18 21:32:47 +1030

    fix mapper

--- mask_former/data/dataset_mappers/mask_former_semantic_dataset_mapper.py
+++ mask_former/data/dataset_mappers/mask_former_semantic_dataset_mapper.py
@@ -172,7 +172,8 @@ class MaskFormerSemanticDatasetMapper:
 
             if len(masks) == 0:
                 # Some image does not have annotation (all ignored)
-                instances.gt_masks = torch.zeros((0, sem_seg_gt.shape[-2], sem_seg_gt.shape[-1]))
+                return None
+                # instances.gt_masks = torch.zeros((0, sem_seg_gt.shape[-2], sem_seg_gt.shape[-1]))
             else:
                 masks = BitMasks(
                     torch.stack([torch.from_numpy(np.ascontiguousarray(x.copy())) for x in masks])
